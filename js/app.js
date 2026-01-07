0993f7702b630e8f70ff062570b5799aaa539edf
Joelthk <joelthehollowknight@gmail.com>
2026-01-07 10:40:19 -0700

    Update app.js
    login config

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-js file
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
+const firebaseConfig = {
+  const firebaseConfig = {
+  apiKey: "AIzaSyCjotIoBJtRrp3p3q6s9TSdZ044pAQoB20",
+  authDomain: "capitol-league.firebaseapp.com",
+  projectId: "capitol-league",
+  storageBucket: "capitol-league.firebasestorage.app",
+  messagingSenderId: "243628029572",
+  appId: "1:243628029572:web:216113ddf24fba4ceab4d1"
+};
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
